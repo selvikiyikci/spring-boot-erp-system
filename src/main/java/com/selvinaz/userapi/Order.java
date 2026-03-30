ef9776fc5452eab7e6d42366e4b8d07169a7253b
selvikiyikci <selvikiyikci@gmail.com>
2026-03-30 13:38:56 +0300

    added an order entity

---
+++ src/main/java/com/selvinaz/userapi/Order.java
@@ -0,0 +1,36 @@
+package com.selvinaz.userapi;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+    private Integer quantity;
+
+    public Order() {
+    }
+
+    public Order(Long id, Long productId, Integer quantity) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+}
