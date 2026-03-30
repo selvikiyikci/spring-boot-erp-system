baeb1b18d27c660954543c2e342754a1e548a257
selvikiyikci <selvikiyikci@gmail.com>
2026-03-30 10:24:15 +0300

    developed UserService to handle application business logic, separating concerns from the controller and using UserRepository for creating and retrieving users

--- src/main/java/com/selvinaz/userapi/UserService.java
+++ src/main/java/com/selvinaz/userapi/UserService.java
@@ -2,10 +2,22 @@ package com.selvinaz.userapi;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public User createUser(User user) {
-        return user;
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
