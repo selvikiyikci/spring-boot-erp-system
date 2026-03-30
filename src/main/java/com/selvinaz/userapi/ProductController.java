25adf1fcf4f76b5c8f0f99fb43a67a2a9a597ea1
selvikiyikci <selvikiyikci@gmail.com>
2026-03-30 11:37:59 +0300

    added ProductController with product REST endpoints

--- src/main/java/com/selvinaz/userapi/ProductController.java
+++ src/main/java/com/selvinaz/userapi/ProductController.java
@@ -1,4 +1,28 @@
 package com.selvinaz.userapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
 }
