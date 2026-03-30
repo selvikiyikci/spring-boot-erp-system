9f394d810caa668724087af2229280ea8eaa15a1
selvikiyikci <selvikiyikci@gmail.com>
2026-03-30 09:39:51 +0300

    added user service layer

--- src/main/java/com/selvinaz/userapi/UserController.java
+++ src/main/java/com/selvinaz/userapi/UserController.java
@@ -1,5 +1,6 @@
 package com.selvinaz.userapi;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,13 @@ public class UserController {
                 new User(2L, "Ayşe"),
                 new User(3L, "Mehmet")
         );
+    }
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 }
---
+++ src/main/java/com/selvinaz/userapi/UserService.java
@@ -0,0 +1,11 @@
+package com.selvinaz.userapi;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public User createUser(User user) {
+        return user;
+    }
+}
