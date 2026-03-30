a1ebcc3b9fc561996a5323fd79557e48b3c3bffa
selvikiyikci <selvikiyikci@gmail.com>
2026-03-30 10:20:22 +0300

    created User entity class representing the data model, annotated with @Entity for database mapping and configured primary key with @Id and @GeneratedValue to enable automatic ID generation

--- src/main/java/com/selvinaz/userapi/User.java
+++ src/main/java/com/selvinaz/userapi/User.java
@@ -1,9 +1,23 @@
 package com.selvinaz.userapi;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    public User() {
+    }
 
     public User(Long id, String name) {
         this.id = id;
---
+++ src/main/java/com/selvinaz/userapi/UserRepository.java
@@ -0,0 +1,4 @@
+package com.selvinaz.userapi;
+
+public class UserRepository {
+}
