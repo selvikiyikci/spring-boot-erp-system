86b64c7f1f4969128d333e541155226ef373b3c4
selvikiyikci <selvikiyikci@gmail.com>
2026-03-30 10:27:15 +0300

    Completed user management API

--- src/main/java/com/selvinaz/userapi/UserController.java
+++ src/main/java/com/selvinaz/userapi/UserController.java
@@ -1,7 +1,8 @@
 package com.selvinaz.userapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -9,20 +10,19 @@ import java.util.List;
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/users")
     public List<User> getUsers() {
-        return List.of(
-                new User(1L, "Ali"),
-                new User(2L, "Ayşe"),
-                new User(3L, "Mehmet")
-        );
+        return userService.getUsers();
     }
+
     @PostMapping("/users")
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
-    }
-    private final UserService userService;
-    public UserController(UserService userService) {
-        this.userService = userService;
     }
 }
